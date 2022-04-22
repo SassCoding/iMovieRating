db901765c385c9dc559f08dd28631930534b9019
RTS <r.sass5554@gmail.com>
2022-04-22 16:18:37 -0500

    Corrected errors
    Fixed create and review pages

--- create.php
+++ create.php
@@ -50,25 +50,29 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Create Page</title>
 </head>
-<body>
-	<?php include('nav.php'); ?>
-    <div class="container" id="movies">
-	<form method="post" class="row g-3 needs-validation" novalidate>
-		<div class="col-md-4">
-			<div class="input-group">
-				<span class="input-group-text">Review</span>
-				<textarea class="form-control" name="content" id="content" aria-label="Review" placeholder="Your Review Here.."></textarea>
-			</div>
-		</div>
-		<div class="col-12">
-			<input type="hidden" name="movieId" value="<?php echo $row['movie_id']?>" />
-			<input type="submit" name="create" value="Create" />
-		</div>
-	</form>
-    </div>
-    <?php include 'footer.php'?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
+<main>
+    <?php include("nav.php");?>
+    <section class="bg-dark text-dark p-5 text-left">
+      <div class="form_bg">
+        <div class="container">
+        <form method="post" class="row g-3 needs-validation" novalidate style="justify-content: center;">
+      <div class="col-md-4">
+        <div class="input-group">
+          <span class="input-group-text">Review</span>
+          <textarea class="form-control" name="content" id="content" aria-label="Review"></textarea>
+        </div>
+        <input type="hidden" name="id" value="<?php echo $row['review_id']?>" />
+        <input type="hidden" name="movieId" value="<?php echo $row['movie_id']?>" />
+        <input type="hidden" name="movieId" value="<?php echo $row['movie_id']?>" />
+		<input class="btn btn-success" type="submit" name="create" value="Create" />
+      </div>
+    </form>
+          </div>  
+      </div>       
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </section>
+</main>
 </html>
